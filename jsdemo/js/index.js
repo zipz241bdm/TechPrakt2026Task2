8c57d4356d7daef0a4a70ef3e6c7545b9093b35b
Dmytro Bilous <zipz241_bdm@student.ztu.edu.ua>
2026-06-12 14:41:51 +0200

    fix: resolve duplicate variable, fix numeric literal, and enable dead code

--- jsdemo/js/index.js
+++ jsdemo/js/index.js
@@ -1,4 +1,3 @@
-
 class User {
     #login;
     #password;
@@ -9,7 +8,6 @@ class User {
     }
 
     getLogin() {
-        console.log(this);
         return this.#login;
     }
 
@@ -29,35 +27,32 @@ class Admin extends User {
         this.#role = role;
     }
 }
+
 const user = new User('admin', '123456');
+const admin = new Admin('admin', '123456', 'all');
 
 let panelParams = {
-    //width : 300,
-    title : 'Title',
-    height : 200,
-    age : 30
+    title: 'Title',
+    height: 200,
+    age: 30,
+
 };
 
-let {title, height} = panelParams;
-let [id, title, count, price] = [1, 'title', 10, 10,5];
+let { title, height } = panelParams;
+let [id, itemTitle, count, price] = [1, 'title', 10, 10.5];
+
 let x = 1;
 let y = 2;
 [x, y] = [y, x];
 
-
-
 function func2(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+    console.log(a, b, c);
 }
-/*let arr2 = [1,2,3,4,5];
-func2(...arr2);*/
 
-/*
+let arr2 = [1, 2, 3, 4, 5];
+func2(...arr2);
+
 let myf = user.getLogin;
 console.log(myf.bind(user));
 
 console.log(user.getLogin());
-const admin = new Admin('admin', '123456', 'all');
-*/
